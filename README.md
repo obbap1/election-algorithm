65102d6700c1fc53edc09aa42b9412b3ecc0397b
Obba Paschal <paschal.obba@stu.cu.edu.ng>
2019-08-09 20:52:14 +0100

    Fix: use sets to handle sorting

---
+++ README.md
@@ -0,0 +1,5 @@
+# The Election Algorithm 
+[![star this repo](http://githubbadges.com/star.svg?user=obbap1&repo=election-algorithm&style=flat)](https://github.com/obbap1/election-algorithm)
+
+The Election algorithm explains how master nodes are selected in distributed systems, read more about it [here]()
+
--- server.js
+++ server.js
@@ -1,49 +1,70 @@
 const amqp = require("amqplib/callback_api");
 const cron = require("node-cron");
-const { spawn } = require("child_process");
 const os = require("os");
 
-//Run heartbeat check every 10 seconds
+//Connect to the IP address of the rabbitmq container
+const url = "amqp://guest:guest@172.22.0.2";
+
+ 
 const sendContainerIdToOthers = () => {
+  /**
+   * method for sending containerId to other nodes
+   * @param {null} 
+   * @returns {null}
+   * 
+*/
   console.log(`My id is ${os.hostname()}`);
-  amqp.connect("amqp://guest:guest@172.22.0.2", (error0, connection) => {
+  amqp.connect(url, (error0, connection) => {
     if (error0) throw error0;
     connection.createChannel((error1, channel) => {
       if (error1) throw error1;
-      const queue = "containerIds";
+      const exchange = "logs";
       const msg = `My id is ${os.hostname()}`;
 
-      channel.assertExchange('logs','fanout', { durable: false });
+      channel.assertExchange(exchange, "fanout", { durable: false });
 
-      channel.publish('logs', '', Buffer.from(msg));
-
-      channel.assertQueue('', {exclusive: true});
-
-      channel.bindQueue('', 'logs', '');
+      channel.publish(exchange, "", Buffer.from(msg));
     });
   });
 };
 
-const sendElectionRequest = () => {};
 
-amqp.connect('amqp://guest:guest@172.22.0.2',(error0, connection) => {
-    if(error0) throw error0;
-    connection.createChannel((error1, channel) => {
-        if(error1) throw error1;
-        const exchange = 'logs';
-        channel.assertExchange(exchange, 'fanout', {durable: false});
+amqp.connect(url, (error0, connection) => {
+  if (error0) throw error0;
+  connection.createChannel((error1, channel) => {
+    if (error1) throw error1;
+    const exchange = "logs";
+    channel.assertExchange(exchange, "fanout", { durable: false });
 
-        channel.assertQueue('containerIds', {exclusive: true}, (error2, q) => {
-            if(error2) throw error2;
-            console.log(`Waiting for messages in ${q.queue}`);
-            channel.bindQueue(q.queue,exchange,'');
-            channel.consume(q.queue, (msg) => {
-                if(msg.content) console.log(`received: ${msg.content.toString()}`);
-            },{
-                noAck: true
-            });
-        });
+    channel.assertQueue("", { exclusive: true }, (error2, q) => {
+      if (error2) throw error2;
+      console.log(`Waiting for messages in ${q.queue}`);
+      channel.bindQueue(q.queue, exchange, "");
+      let resultSet = new Set();
+      //Clear the set every 15 seconds
+      setInterval(()=>{resultSet = new Set()}, 15000);
+      channel.consume(
+        q.queue,
+        msg => {
+          if (msg.content) {
+            console.log(`received: ${msg.content.toString()}`);
+            const id = msg.content
+              .toString()
+              .split("is")[1]
+              .trim();
+            resultSet.add(id);
+            console.log("Container id's", resultSet);
+            const findMaster = Array.from(resultSet).sort();
+            console.log(`Our Master Node is ${findMaster[0]}`);
+          }
+        },
+        {
+          noAck: true
+        }
+      );
     });
+  });
 });
 
-cron.schedule('10 * * * * *', () => sendContainerIdToOthers());
+//Run every 10 seconds
+cron.schedule("10 * * * * *", () => sendContainerIdToOthers());
